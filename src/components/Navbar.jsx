dd11ab55f751be6441e124aab896ca84c692dfc1
tawiah <tawiahin4k@gmail.com>
2024-12-16 17:13:25 +0000

    refactor Navbar component and update CSS class naming for consistency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
-import { appleImg } from '../utils'
+import { appleImg, bagImg, searchImg } from '../utils'
+import { navLists } from '../constants'
 
 const Navbar = () => {
   return (
-      <header>
-          <nav>
-              <img src={appleImg} alt='Apple' height={18} width={14} />
+    <header className='w-full py-5 sm:px-10 px-5 flex justify-between items-center'>
+      <nav className='flex w-full screen-max-width'>
+        <img src={appleImg} alt="Apple" height={18} width={14} />
 
-              <div>
-                  {['Phones', 'Macbooks', 'iPads', 'Accessories'].map((nav, i) => (
-                      <div key={nav}>
-                          {nav}
-                      </div>
-                  ))}
-              </div>
+        <div className='flex flex-1 justify-center max-sm:hidden'>
+          {navLists.map((nav, i) => (
+            <div className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all' key={nav}>{nav}</div>
+          ))}
+        </div>
 
-              <div>
-                  
-              </div>
-          </nav>
-      </header>
-  )
+        <div className='flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1'>
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar
