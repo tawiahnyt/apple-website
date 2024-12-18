16756c191880f110f80849ee31c5d7228e8eefb6
tawiah <tawiahin4k@gmail.com>
2024-12-18 09:56:23 +0000

    refactor: enhance GSAP animation logic in Model component; fix className syntax in ModelView; implement animation utility function

--- src/utils/animation.js
+++ src/utils/animation.js
@@ -1,3 +1,34 @@
-export const animateWithGsapTimeline = () => {
-    
-}
+/** @format */
+
+export const animateWithGsapTimeline = (
+  timeline,
+  rotationRef,
+  rotationState,
+  firstTarget,
+  seconfTarget,
+  animationProps
+) => {
+  timeline.to(rotationRef.current.rotation, {
+    y: rotationState,
+    duration: 1,
+    ease: "power2.inOut",
+  });
+
+  timeline.to(
+    firstTarget,
+    {
+      ...animationProps,
+      ease: "power2.inOut",
+    },
+    "<"
+  );
+
+  timeline.to(
+    seconfTarget,
+    {
+      ...animationProps,
+      ease: "power2.inOut",
+    },
+    "<"
+  );
+};
